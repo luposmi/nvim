7ffc6c7ca7e7d4026730f6b09f71ff2315c887af
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-02-24 11:47:07 +0100

    remove oil plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,12 @@
 return {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-        }
-    },
-    {
-        'stevearc/oil.nvim',
-        opts = {},
-        -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
     }
+}
