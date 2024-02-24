ea98a40a66d2599d9eeb3915a6203887d01a0bf7
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-02-24 10:39:14 +0100

    Do Lazy plugins for LSP and Highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+return{
+    "nvim-treesitter/nvim-treesitter",
+    event = {"BufReadPre","BufNewFile"},
+    build = ":TSUpdate",
+    dependencies = {"nvim-treesitter/nvim-treesitter-context",
+        "nvim-treesitter/nvim-treesitter-textobjects"},
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = { "asm","awk","bash","bibtex","c","clojure","cmake","commonlisp","cpp","css","csv","dart","diff","devicetree","diff","disassembly","dockerfile","eex","elixir","gdscript","git_config","git_rebase","gitattributes","gitcommit","gitignore","go","godot_resource", "haskell","hlsplaylist","html","http","java","javascript","json","latex","linkerscript","llvm","lua","markdown","matlab","meson","ninja","nix","ocaml","printf",
+            "properties", -- java
+            "python","regex","rust","scss","sql",
+            "tablegen", -- llvm
+            "toml","typescript","verilog","vim","vimdoc","xml","yaml","zig"
+            },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+          autotag = { enable = true },
+          incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                }
+            }
+        })
+    end
+},{
+  "nvim-treesitter/nvim-treesitter-context",
+  config = function()
+    require("treesitter-context").setup()
+  end
+}
---
+++ lua/remap.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "open the explorer" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves the current line up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves the current line down" })
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "append next line to current line, don't move cursor" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "half page up, cursor in middle" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "half page down, cursor in middle" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "repeat Search stay in middle" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "repeat Search opposide stay in middle" })
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "paste without replacing buffer" })
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "copy to clippboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "copy to clippboard" })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "delete to void" })
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format from lsp"})
+
+
+-- quickfix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace current item"})
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "set current file to executable"})
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end, { desc = "apply current file, aka so"})
